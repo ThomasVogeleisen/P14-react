786c4cc7ab9d1856cfb85beb80eef7a9b3cdbd4d
Thomas Vogeleisen <80261234+ThomasVogeleisen@users.noreply.github.com>
2024-06-21 19:27:13 +0200

    mise en place du select

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Employe } from './pages/Employe'
 
 function App() {
   return (
-    <>
-      <p>Hello Thomas</p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/employe" element={<Employe />} />
+      </Routes>
+    </Router>
   )
 }
 
